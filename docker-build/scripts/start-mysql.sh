b9320828887c9b60eb7f44a8943102fffddf7ff5
lovehzj <1922919664@qq.com>
2023-02-12 22:24:07 +0800

    fix conf file

--- docker-build/scripts/start-mysql.sh
+++ docker-build/scripts/start-mysql.sh
@@ -8,9 +8,12 @@ rm -rf /etc/mysql/my.cnf
 rm -rf /etc/mysql/conf.d
 rm -rf /etc/mysql/mysql.conf.d
 CONF_FILE=/etc/mysql/my.cnf
-echo "[mysqld]" >/etc/mysql.conf
-echo "bind-address = 0.0.0.0" >>/etc/mysql.conf
-echo "mysqlx-bind-address = 0.0.0.0" >>/etc/mysql.conf
-echo "ssl = 0" >>/etc/mysql.conf
+echo "[mysqld]" >$CONF_FILE
+echo "bind-address = 0.0.0.0" >>$CONF_FILE
+echo "mysqlx-bind-address = 0.0.0.0" >>$CONF_FILE
+echo "ssl = 0" >>$CONF_FILE
+echo "log_error = $MYSQL_HOME/logs/mysql_error.log" >>$CONF_FILE
+echo "[mysqldump]" >>$CONF_FILE
+echo "quick" >> $CONF_FILE
 
 mysql -u root < $MYSQL_HOME/mate/sql/init.sql
